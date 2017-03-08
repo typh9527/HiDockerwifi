0714a7da22060500e0c9469891382040c02d4c88
joliu <joliu@s-an.org>
2017-03-08 20:18:11 +0800

    设备接入客户端

---
+++ client/tcpclient.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+#coding=utf-8
+import socket
+import sys
+import json
+
+def client(ip,port,message):
+    """
+    设备接入函数
+    """
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((ip,port))
+    try:
+        print("Send:"+message)
+        s.sendall(message.encode())
+        response = s.recv(1024).decode()
+        jresp = json.loads(response)
+        print("Recv:"+str(jresp))
+
+    finally:
+        s.close()
+
+if __name__ == "__main__":
+    #:配置host和port
+    HOST,PORT = "192.168.12.1",22223
+    #:构建发送消息
+    msg1 = [{'cmd':r'/usr/bin/top -b','equip':"bx", 'log':"up",'repo':"joliu/mysql_client",'imname':"hiwifi/client","dcport":"33332"}]
+
+    #:将json转化成可发送数据
+    jmsg1 = json.dumps(msg1)
+    client(HOST,PORT,jmsg1)
